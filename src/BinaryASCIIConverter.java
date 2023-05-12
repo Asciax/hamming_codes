f63f2d34c252fd5ddd41f5d27e4d9e0afe0e3532
Asciax <rcaterev@gmail.com>
2023-05-12 15:14:03 -0400

    Completed binary -> ascii and ascii -> binary converter utility class

--- src/BinaryASCIIConverter.java
+++ src/BinaryASCIIConverter.java
@@ -1,40 +1,93 @@
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
+// Author : Asciax
+// May 2023
+
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.nio.charset.Charset;
 
 final class BinaryASCIIConverter {
     // Utility class to convert ASCII -> binary and binary -> ASCII
-    private static String charset = "US-ASCII";
+    private static Charset charsetObj = Charset.forName("Cp437");
+    private static String charsetName = "Cp437";
 
     private BinaryASCIIConverter(){} // private constructor to avoid instantiation
 
-    public static String convertToASCII(String input){
-        return "";
+    public static String getCharsetName(){
+        return charsetName;
+    }
+    /* ---setCharset(String charsetName)---
+     * String charsetName -> Accepted charset names in the Charset class from java.nio.charset.Charset.
+     *                    -> If charsetName is invalid, throws Exception.
+     */
+    public static void setCharset(String charsetName)
+            throws IllegalCharsetNameException, IllegalArgumentException, UnsupportedCharsetException
+    {
+            charsetObj = Charset.forName(charsetName);
+            BinaryASCIIConverter.charsetName = charsetName;
     }
 
-    public static int[] convertToBinary(String input){
-        String ascii_version = input;
+    /*---convertToASCII(int[] input)---
+     * int[] input -> Raw stream/array of bits
+     * Returns : String containing (charsetName) characters
+     * O(n) runtime because of the binary check.
+     */
+    public static String convertToASCII(int[] input) throws IllegalArgumentException{
+        for (int i : input){
+            if (i != 1 && i != 0){
+                throw new IllegalArgumentException("Stream has to contain only binary");
+            }
+        }
+        int nb_bytes = input.length/8;
+        int index=0;
+        char[] char_arr = new char[nb_bytes];
+
+        for (int bitindex = 0; index < nb_bytes; index++, bitindex+=8 ){
+            int val = 128*input[bitindex] + 64*input[bitindex+1] + 32*input[bitindex+2] + 16*input[bitindex+3]
+                    + 8*input[bitindex+4] + 4*input[bitindex+5] + 2*input[bitindex+6] + input[bitindex+7];
+            char_arr[index] = (char) val;
+        }
+
+        return String.valueOf(char_arr);
+    }
+
+
+    // Converts ASCII String to binary, outputs binary array
+    public static int[] convertToBinary(String input) throws IllegalArgumentException{
+        if (input == null || input.length() == 0){
+            throw new IllegalArgumentException("Input string cannot be null or empty.");
+        }
+
         int[] output = new int[input.length()*8];
         byte[] byte_arr;
+
+        // Verifying that the charset is supported, if not defaulting to ASCII.
         try {
-            byte_arr = ascii_version.getBytes(charset);
+            byte_arr = input.getBytes(charsetObj);
+
         }
-        catch(UnsupportedEncodingException e){
-            byte_arr = ascii_version.getBytes();
+        catch(Exception e){
+            byte_arr = input.getBytes();
         }
+
+
         int tempindex = 0;
         for (byte current: byte_arr){
-            byte temp_byte = current;
+            int temp_byte = current;
             for (int i = 0; i < 8; i++){
                 output[tempindex] = (temp_byte & 128 ) == 0 ? 0 : 1;
                 tempindex++;
-                temp_byte = (byte) (temp_byte << 1);
+                temp_byte = (temp_byte << 1);
             }
         }
         return output;
     }
-
+    /*
     public static void main(String[] args){
-        //int[] test = BinaryASCIIConverter.convertToBinary("real");
-        //System.out.println(Arrays.toString(test));
+        String rawstring = "REAL";
+        int[] test = BinaryASCIIConverter.convertToBinary(rawstring);
+        System.out.println(Arrays.toString(test));
+        String otherside = BinaryASCIIConverter.convertToASCII(test);
+        System.out.println(otherside);
     }
+    */
 }
