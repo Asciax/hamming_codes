f3fc352d5b9ae2b21a1762e4574c7d9bea5a5ecd
Asciax <rcaterev@gmail.com>
2023-06-21 16:02:23 -0400

    Overloaded Encoder with Integer array bitstream in option. Remover tester class.

--- src/Encoder.java
+++ src/Encoder.java
@@ -10,13 +10,13 @@ public class Encoder {
     private String raw_msg;
 
     // Raw bitstream of non-encoded message.
-    private Integer[] bitstream;
+    private Integer[] bitstream_in;
 
     // LinkedList used as a queue in encode()/
     private LinkedList<Integer[]> nibbleQueue;
 
-    // Final encoded result presented as Integer ArrayList.
-    private ArrayList<Integer> raw_final;
+    // Final encoded bitstream presented as Integer ArrayList.
+    private ArrayList<Integer> bitstream_out;
 
     // Hamming encoding in string version.
     private String final_code;
@@ -32,38 +32,83 @@ public class Encoder {
      *        [ 0 0 0 1 ]
      */
     private final Integer[] seven_four_generator_matrixT = {13, 11, 8, 7, 4, 2, 1};
+
+    public Integer[] getBitstream_in(){
+        return this.bitstream_in.clone();
+    }
+    public void setBitstream_in(Integer[] bitstream)
+            throws IndexOutOfBoundsException, IllegalArgumentException{
+        if (bitstream == null){
+            throw new IllegalArgumentException("Bitstream can't be empty");
+        }
+        else if ((bitstream.length % 4) != 0){
+            throw new IndexOutOfBoundsException("Bitstream must be a multiple of 4");
+        }
+        else {
+            for (Integer val : bitstream){
+                if (val != 0 && val != 1){
+                    throw new IllegalArgumentException("Bitstream should only contain binary");
+                }
+            }
+            this.bitstream_in = bitstream;
+        }
+    }
+    public Integer[] getBitstreamOut(){
+        Integer[] temp = new Integer[this.bitstream_out.size()];
+        return this.bitstream_out.toArray(temp);
+    }
+
+    public String getCodeString(){
+        return new String(this.final_code);
+    }
     public Encoder(String raw_message){
         this.raw_msg = raw_message;
         this.nibbleQueue = new LinkedList<Integer[]>();
-        this.raw_final = new ArrayList<Integer>();
+        this.bitstream_out = new ArrayList<Integer>();
+        this.__convertRawToBitstream__();
     }
 
+    public Encoder(Integer[] bitstream_in){
+        this.setBitstream_in(bitstream_in);
+        this.raw_msg = null;
+        this.nibbleQueue = new LinkedList<Integer[]>();
+        this.bitstream_out = new ArrayList<Integer>();
+    }
+    private void __convertRawToBitstream__(){
+        this.bitstream_in = BinaryASCIIConverter.convertToBinary(this.raw_msg);
+    }
+
+
+    /* encode():
+     *  Main function of the class, encodes bitstream_in using Hamming[7,4] code.
+     */
     public void encode(){
-        this.bitstream = BinaryASCIIConverter.convertToBinary(this.raw_msg);
 
         // Nibble queue to be used by a more general Hamming code algorithm
-        for (int i = 0, j = 0; i < (this.bitstream.length / 4); i++, j+=4){
-            this.nibbleQueue.addFirst( Arrays.copyOfRange(this.bitstream, j, j+4));
+        for (int i = 0, j = 0; i < (this.bitstream_in.length / 4); i++, j+=4){
+            this.nibbleQueue.addFirst( Arrays.copyOfRange(this.bitstream_in, j, j+4));
         }
 
         while (!this.nibbleQueue.isEmpty()){
             Integer[] current_nibble = this.nibbleQueue.removeLast();
-            //ArrayList<Integer> hamming_nibble = new ArrayList<Integer>();
+
             int nibble_value = nibbleToInt(current_nibble);
             for (int i = 0 ; i < 7; i++){
                 // The matrix mult. result mod 2 is the same as the population count of "1" bits when bitwise ANDing
-                // each row from the generator with the nibble column. Solution from wikipedia.
-                this.raw_final.add((Integer.bitCount(this.seven_four_generator_matrixT[i] & nibble_value)) % 2);
+                // each row from the generator with the nibble column.
+                this.bitstream_out.add((Integer.bitCount(this.seven_four_generator_matrixT[i] & nibble_value)) % 2);
             }
 
         }
-        StringBuilder strBuilder = new StringBuilder(this.raw_final.size());
-        for (Integer integer : this.raw_final) {
+        StringBuilder strBuilder = new StringBuilder(this.bitstream_out.size());
+        for (Integer integer : this.bitstream_out) {
             strBuilder.append(integer);
         }
         this.final_code = strBuilder.toString();
     }
-
+    /* nibbleToInt(Integer[] nibble):
+     *  Converts a 4-bit binary sequence to its decimal int.
+     */
     private int nibbleToInt(Integer[] nibble) throws IllegalArgumentException{
         if (nibble.length != 4){
             throw new IllegalArgumentException("A nibble has 4 bits, please input an array of length 4");
@@ -77,29 +122,4 @@ public class Encoder {
         return nibble[0]*8 + nibble[1]*4 + nibble[2]*2 + nibble[3];
     }
 
-    public Integer[] getCodeArray(){
-        Integer[] temp = new Integer[this.raw_final.size()];
-        return this.raw_final.toArray(temp);
-    }
-
-    public String getCodeString(){
-        return new String(this.final_code);
-    }
-
-    public static void main(String[] args) {
-        String test = "HelloWorld?";
-        Encoder encoderV1 = new Encoder(test);
-        encoderV1.encode();
-        System.out.println(Arrays.toString(encoderV1.bitstream));
-        for (int i  = 0; i < (encoderV1.bitstream.length); i+=4){
-            System.out.print(
-                    Arrays.toString(Arrays.copyOfRange(encoderV1.bitstream, i, i+4))
-            );
-
-            System.out.print(" , ");
-        }
-        System.out.println("\n" + Arrays.toString(encoderV1.getCodeArray()));
-        System.out.println(encoderV1.getCodeString());
-
-    }
 }
