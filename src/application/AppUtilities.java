c5670589a4769dc8a48bb79ac2a49d5a41520b26
Asciax <rcaterev@gmail.com>
2023-08-25 20:03:32 -0400

    Generalized addElement to apply to all necessary types of nodes

--- src/application/AppUtilities.java
+++ src/application/AppUtilities.java
@@ -2,18 +2,26 @@ package application;
 
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.Pane;
 
 class AppUtilities {
 
 
-    static boolean addElement(Group parent, Object toAdd){
+    static boolean addElement(Parent parent, Object toAdd){
         /*
-         * addElement(Group, Node toAdd)
-         *  Adds an elements to the desired group node.
+         * addElement(Group or Pane, Node toAdd)
+         *  Adds an elements to the desired group or pane node.
          *  Upon failure, returns false.
          */
         if (Node.class.isAssignableFrom(toAdd.getClass())){
-            parent.getChildren().add((Node) toAdd);
+            if (Group.class.isAssignableFrom(parent.getClass())){
+                ((Group) parent).getChildren().add((Node) toAdd);
+            }
+            else if (Pane.class.isAssignableFrom(parent.getClass())){
+                ((Pane) parent).getChildren().add((Node) toAdd);
+            }
             return true;
         }
         else{
